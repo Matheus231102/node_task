96dda49547e5cfc55acf9c4ac53fe99e5b8c4757
Matheus231102 <matheusbadia23@gmail.com>
2023-05-28 23:50:06 -0300

    exclusão de arquivo teste

--- teste.js
+++
@@ -1,7 +0,0 @@
-fetch('http://127.0.0.1:9999/task_post', {
-    method: 'post',
-    body: JSON.stringify({ "task_name": "testando método pelo arq js" }),
-    headers: {
-        'Content-Type': 'application/json'
-    }}
-)
